7d3827c73ed6cf514b2b2b6ad46b31a2d8975633
Antonov Andrei <aaakuzbass@gmail.com>
2023-02-09 17:38:54 +0400

    hw_2

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -11,10 +11,12 @@ public class MainTestArrayStorage {
         Resume r1 = new Resume("uuid1");
         Resume r2 = new Resume("uuid2");
         Resume r3 = new Resume("uuid3");
+        Resume r4 = new Resume("uuid4");
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
+        ARRAY_STORAGE.save(r2);
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.getSize());
@@ -25,6 +27,7 @@ public class MainTestArrayStorage {
         ARRAY_STORAGE.delete(r1.getUuid());
         printAll();
         ARRAY_STORAGE.update(r2);
+        ARRAY_STORAGE.update(r4);
         printAll();
         ARRAY_STORAGE.clear();
         printAll();
