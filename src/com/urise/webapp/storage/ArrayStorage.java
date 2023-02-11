e0e0536e05d98a64b71c31fce4788c9918b629c8
Antonov Andrei <aaakuzbass@gmail.com>
2023-02-11 11:06:41 +0400

    hw_2 fix

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 public class ArrayStorage {
 
     private int size;
-    Resume[] storage = new Resume[10000];
+    private final Resume[] storage = new Resume[10000];
 
     public int getSize() {
         return size;
@@ -18,69 +18,52 @@ public class ArrayStorage {
 
     public Resume get(String uuid) {
         int index = findPosition(uuid);
-
-        if (index >= 0) {
-            return storage[index];
+        if (index < 0) {
+            System.out.printf("ОШИБКА. Резюме c id \"%s\" отсутствует в хранилище.%n", uuid);
+            return null;
         }
-
-        System.out.printf("ОШИБКА. Резюме c id \"%s\" отсутствует в хранилище.%n", uuid);
-        return null;
+        return storage[index];
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        if (size > 0) {
-            return Arrays.copyOf(storage, size);
-        }
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     public void save(Resume r) {
-        if (findPosition(r.getUuid()) >= 0) {
-            System.out.printf("ОШИБКА. Резюме c id \"%s\" уже есть в хранилище.%n", r.getUuid());
-            return;
-        }
-
         if (size >= storage.length) {
             System.out.println("ОШИБКА. База резюме переполнена.");
-            return;
+        } else if (findPosition(r.getUuid()) >= 0) {
+            System.out.printf("ОШИБКА. Резюме c id \"%s\" уже есть в хранилище.%n", r.getUuid());
+        } else {
+            storage[size++] = r;
         }
-
-        storage[size++] = r;
     }
 
     public void delete(String uuid) {
         int index = findPosition(uuid);
-
         if (index < 0) {
-            System.out.printf("ОШИБКА. Резюме c id \"%s\" уже отсутствует в хранилище.%n", uuid);
+            System.out.printf("ОШИБКА. Резюме c id \"%s\" отсутствует в хранилище.%n", uuid);
             return;
         }
-
-        if (index < storage.length - 1) {
-            System.arraycopy(storage, index + 1, storage, index, size - 1 - index);
-        }
-        storage[--size] = null;
+        storage[index] = storage[--size];
+        storage[size] = null;
     }
 
     public void update(Resume resume) {
         int index = findPosition(resume.getUuid());
-
         if (index < 0) {
             System.out.printf("ОШИБКА. Резюме c id \"%s\" уже отсутствует в хранилище.%n", resume.getUuid());
             return;
         }
-
         storage[index] = resume;
     }
 
     public void clear() {
-        if (size > 0) {
-            Arrays.fill(storage, 0, size, null);
-            size = 0;
-        }
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     private int findPosition(String uuid) {
@@ -89,7 +72,6 @@ public class ArrayStorage {
                 return i;
             }
         }
-
         return -1;
     }
 }
